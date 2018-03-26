4cba3af102d62f6a7df339b2b7eac750579ae403
wei.fanguang <wei.fanguang@datatom.com>
2018-03-26 17:13:56 +0800

    add:新建v0.1.0分支，并提交已在windows下（本地）成功运行的代码

--- lessons/01-setting-up/modules/App.js
+++ lessons/01-setting-up/modules/App.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import './../public/index.less'
 
 export default React.createClass({
   render() {
-    return <div>Hello, React Router!</div>
+    return (
+        <div id='root' className='root'>
+          <div>Hello, React Router!</div>
+          <p className='name'>第一个react router应用</p>
+          <p className='date'>
+              2018年3月20日
+              <span>星期二 上午 <a href="#">10点</a></span>
+          </p>
+          <p className='tips'>新增</p>
+	</div>
+    )
   }
 })
--- lessons/01-setting-up/webpack.config.js
+++ lessons/01-setting-up/webpack.config.js
@@ -1,3 +1,6 @@
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './index.js',
 
@@ -6,9 +9,31 @@ module.exports = {
     publicPath: ''
   },
 
+  devtool: 'eval-source-map',
+
+  devServer: {
+      contentBase: "./public",      //本地服务器加载的页面所在的目录
+      historyApiFallback: true,     //不跳转
+      inline: true,
+      hot: true                     //热加载
+  },
+
   module: {
     loaders: [
-      { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader?presets[]=es2015&presets[]=react' }
+      { 
+        test: /\.js$/, 
+        exclude: /node_modules/, 
+        loader: 'babel-loader?presets[]=es2015&presets[]=react'
+      },
+      {
+        test: /\.less$/,
+        loader: 'style-loader!css-loader!less-loader'
+      }
     ]
-  }
+  },
+
+  plugins: [
+      new webpack.HotModuleReplacementPlugin()          //热加载插件
+  ]
+
 }
